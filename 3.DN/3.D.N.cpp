9106287d74ab2e18c31fb8384cbedacc3c884ce9
markkrajzel <146818091+markkrajzel@users.noreply.github.com>
2023-11-23 13:08:31 +0100

    3.DN
    Aprox. integrala

---
+++ 3.DN/3.D.N.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+//Izračun arctan(x) s pomočjo Taylorjeve vrste
+double arctan(double x, int iteracije) {
+    double TV = 0.0;
+    double t2;
+    double t1;
+
+    for (double n = 0; n <= iteracije; n++) {
+        t1 = pow(x, 2 * n + 1) * pow(-1, n);
+        t2 = t1 / (2 * n + 1);
+        TV += t2;
+    }
+
+    return TV;
+}
+
+
+//Izračun e^3x
+double eksp_3x(double x) {
+    return exp(3 * x);
+ }
+
+//Funkcija za integracijo s trapezno metodo
+double trapez_int (double (*f)(double), double a, double b, int n) {
+    double h = (b - a) / n;
+    double rez = 0.5 * (f(a) + f(b));
+
+    for (int i = 1; i < n; ++i) {
+        double x = a + i * h;
+        rez += f(x);
+    }
+    return h * rez;
+}
+
+
+int main() {
+    double pi = acos(-1);
+    double a = 0;
+    double b = pi / 4;
+    int n = 1000;
+
+    double rez = trapez_int([](double x) {
+        return eksp_3x(x) * arctan(x / 2.0, 100);
+        }, a, b, n);
+    
+    std::cout << "Vrednost integrala: " << rez << std::endl;
+    return 0;
+
+}
